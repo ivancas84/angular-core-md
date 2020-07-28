15a4e973ccc74789972a5b21d88621069b2ed421
ivancas84 <ivancas84@gmail.com>
2020-07-28 18:12:16 -0300

    Refactorizar componente autocompletar

--- component/input-autocomplete/input-autocomplete.component.ts
+++ component/input-autocomplete/input-autocomplete.component.ts
@@ -1,4 +1,4 @@
-import { Input, OnInit, Component} from '@angular/core';
+import { Input, OnInit, Component, OnChanges, SimpleChanges, DoCheck} from '@angular/core';
 import { FormGroup, FormControl, FormGroupDirective, NgForm } from '@angular/forms';
 import { Observable, Subscription, of } from 'rxjs';
 import { Router } from '@angular/router';
@@ -13,7 +13,7 @@ import { Display } from '../../class/display';
   selector: 'app-input-autocomplete',
   templateUrl: './input-autocomplete.component.html',
 })
-export class InputAutocompleteComponent implements  OnInit {
+export class InputAutocompleteComponent implements  OnInit, DoCheck {
   /**
    * Componente de administración de fieldset. Características:
    *   El formulario y los datos son definidos en componente principal  
@@ -36,10 +36,20 @@ export class InputAutocompleteComponent implements  OnInit {
   constructor(
     public dd: DataDefinitionService, 
     public storage: SessionStorageService, 
-  ) { }
+  ) { 
+
+  }
+
+  ngDoCheck(): void {
+    if(this.field.errors && !this.searchControl.errors) this.searchControl.setErrors(this.field.getError);
+    if(this.field.dirty && !this.searchControl.dirty) this.searchControl.markAsDirty();
+
+  }
 
   ngOnInit(): void {
-    
+
+    this.searchControl.setValidators(this.field.validator)
+
     this.filteredOptions = this.searchControl.valueChanges.pipe(
       startWith(""),
       tap(value => {
@@ -49,9 +59,13 @@ export class InputAutocompleteComponent implements  OnInit {
       }),
       debounceTime(300),
       distinctUntilChanged(),
-      tap(() => this.field.markAsPending()),
-      mergeMap(value =>  (typeof value == "string" ) ? this._filter(value) : of([])),
-      tap(() => this.field.markAsDirty())
+      mergeMap(value => {
+        if (typeof value == "string" ) return this._filter(value)
+        else {
+          this.searchControl.disable();
+          return of([])
+        }
+      })
     )
 
     this.load$ = this.field.valueChanges.pipe(
@@ -65,7 +79,7 @@ export class InputAutocompleteComponent implements  OnInit {
           }
           return true;
         }
-      )
+      ),
     );
   }
   
@@ -74,10 +88,10 @@ export class InputAutocompleteComponent implements  OnInit {
       row => {
         if(row) { 
           this.searchControl.setValue(row);
-          //this.searchControl.disable();
+          this.searchControl.disable();
         } else {
           this.searchControl.setValue("");
-           //this.searchControl.enable();
+          this.searchControl.enable();
         }
       }
     );
@@ -85,6 +99,7 @@ export class InputAutocompleteComponent implements  OnInit {
 
   private _filter(value: string): Observable<any> {
     if(value === "") return of([]);
+    //this.field.markAsPending();
 
     var display = new Display();
     display.addCondition(["nombre","=~",value]);
