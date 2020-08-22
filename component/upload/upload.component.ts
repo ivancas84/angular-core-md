ac8a2e304ebe7f88ccef9852cac9c81b76b7181d
ivancas84 <ivancas84@gmail.com>
2020-08-22 17:24:25 -0300

    Nuevo componente principal upload

--- component/upload/upload.component.ts
+++ component/upload/upload.component.ts
@@ -1,86 +1,112 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
-import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
-import { first } from 'rxjs/operators';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { SessionStorageService } from '@service/storage/session-storage.service';
-import { UPLOAD_URL } from 'app/app.config';
-
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-upload',
+  selector: 'core-upload',
   templateUrl: './upload.component.html',
 })
-export class UploadComponent implements OnInit {
-
-  @Input() field: FormControl;
-  
-
+export class UploadComponent {
   /**
-   * Fieldset al que pertenece fieldName
-   * Al cargar y procesar el archivo se asignara posteriormente el id resultante a fieldset.fieldName
+   * Comportamiento basico para subir archivos
+   * La idea es subir un archivo que sera procesado en un controlador
+   * La idea es sobrescribir esta clase para modificar la propiedad entityName y seleccionar el controlador correspondiente de procesamiento
+   * El componente no define si se almacena o no el archivo
+   * Utilizado habitualmente para archivos de texto plano con un formato particular que el controlador entiende
    */
 
-  @Input() readonly?: boolean = false;
+  uploadForm: FormGroup = this.fb.group(
+    {
+      file: [null, {
+        validators: [Validators.required],
+      }],
+    }
+  );
 
-  @Input() type?: string = "file";
   /**
-   * Tipo de procesamiento
-   * Permite seleccionar una alternativa entre diferentes controladores de procesamiento
+   * Formulario principal
    */
 
-  fileControl: FormControl = new FormControl();
+  readonly title?: string = "Archivo";
+
+  readonly entityName: string = "file";
+  /**
+   * La entidad hace referencia principalmente al controlador que procesara el archivo
+   */
   
-  file: any = null;
+  isSubmitted: boolean = false;
+  /**
+   * Flag para habilitar/deshabilitar boton aceptar
+   */
+
+  formData: FormData;
+  /**
+   * Objeto con el archivo que sera enviado al servidor
+   */
 
   protected subscriptions = new Subscription();
+   
+  constructor(
+    protected fb: FormBuilder, 
+    protected dd: DataDefinitionService, 
+    protected dialog: MatDialog,
+    protected snackBar: MatSnackBar,
+    protected location: Location, 
+  ) {}
 
-  constructor(protected dd: DataDefinitionService, protected storage: SessionStorageService) { }
+  get file() { return this.uploadForm.get('file')}
 
- ngOnInit(): void {
-    //if(this.field.value) this.initValue(this.field.value);
+  back() { this.location.back(); }
 
-    var s = this.field.valueChanges.subscribe(
-      value => this.initValue(value)
-    );
-
-    this.subscriptions.add(s);
-  }
-
-  initValue(value){
-    this.dd.getOrNull("file", value).pipe(first()).subscribe(
-      row => {
-        if(row) {
-          this.file = row;
-          this.file["link"] = UPLOAD_URL+this.file.content; 
-          this.fileControl.setValue("");
-          this.fileControl.disable();
-        } else {
-          this.file = null;
-          if(!this.readonly) this.fileControl.enable();
-        }
+  upload(): void {
+    /**
+     * subida de archivo
+     * Se define un metodo independiente para facilitar la redefinicion
+     * @return "datos de respuesta (habitualmente array con la informacion del archivo)"
+     */
+    this.dd.upload(this.formData).subscribe(
+      (res) => {
+        console.log(res);
+        this.snackBar.open("Archivo subido", "X");
+      },
+      (err) => {  
+        this.dialog.open(DialogAlertComponent, {
+          data: {title: "Error", message: err.error}
+        });
       }
     );
   }
 
+  reset(): void{
+    this.file.setValue(null);
+  }
+  
   onFileSelect(event) {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
-      const formData = new FormData();
-      formData.append(this.type, file);
-      this.field.markAsPending();
-      this.dd.upload(formData).subscribe(
-        (res) => {
-          this.storage.setItem("file" + res.id, res);
-          this.field.setValue(res.id);
-          this.field.markAsDirty();
-          // this.field.setErrors({'incorrect': true});
-        },
-        (err) => {  
-          console.log(err);
-        }
-      );
+      this.formData = new FormData();
+      this.formData.append(this.entityName, file);
     }
   }
 
+  onSubmit(): void {
+    this.isSubmitted = true;
+    
+    if (!this.uploadForm.valid) {
+      const dialogRef = this.dialog.open(DialogAlertComponent, {
+        data: {title: "Error", message: "El formulario posee errores."}
+      });
+      this.isSubmitted = false;
+
+    } else {
+      this.upload();
+    }
+  }
+
+  ngOnDestroy () { this.subscriptions.unsubscribe() }
 }
