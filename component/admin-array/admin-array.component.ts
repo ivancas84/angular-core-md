bd4ded075570fcc98831a30604309cbd7c5b1968
ivancas84 <ivancas84@gmail.com>
2020-09-24 19:53:02 -0300

    Nuevos componentes para manipular arrays

---
+++ component/admin-array/admin-array.component.ts
@@ -0,0 +1,84 @@
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ReplaySubject, Subscription, Observable } from 'rxjs';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataDefinitionService } from '../../service/data-definition/data-definition.service';
+import { first } from 'rxjs/operators';
+import { Location } from '@angular/common';
+import { emptyUrl } from '../../function/empty-url.function';
+import { SessionStorageService } from '../../service/storage/session-storage.service';
+import { isEmptyObject } from '../../function/is-empty-object.function';
+import { OnInit, AfterViewInit, Component, ChangeDetectionStrategy } from '@angular/core';
+import { markAllAsDirty } from '../../function/mark-all-as-dirty';
+import { logValidationErrors } from '../../function/log-validation-errors';
+import { DialogAlertComponent } from '../dialog-alert/dialog-alert.component';
+
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AdminComponent } from '@component/admin/admin.component';
+
+@Component({
+  selector: 'core-admin-array',
+  template: '',
+changeDetection: ChangeDetectionStrategy.OnPush
+
+
+})
+export abstract class AdminArrayComponent extends AdminComponent {
+/**
+ * Formulario de administracion para un conjunto de datos
+ * Es similiar a AdminComponent pero tiene ciertas restricciones para trabajar con un conjunto de datos
+ */
+
+  constructor(
+    protected fb: FormBuilder, 
+    protected route: ActivatedRoute, 
+    protected router: Router, 
+    protected location: Location, 
+    protected dd: DataDefinitionService, 
+    protected storage: SessionStorageService,
+    protected dialog: MatDialog,
+    protected snackBar: MatSnackBar,
+  ) {
+    super(fb,route, router, location, dd, storage, dialog, snackBar)
+  }
+    
+  setParams(params: any){
+    if(params.hasOwnProperty("ids") && params["ids"]) {
+      this.params = (Array.isArray(params["ids"])) ? params["ids"] : [params["ids"]]
+    } else {
+      this.params = null;
+    } 
+  }
+
+  setData(): void {
+    if(isEmptyObject(this.params)) {
+      this.data$.next([]);
+      return;
+    } 
+
+    this.dd.getAll(this.entityName, this.params).pipe(first()).subscribe(
+      response => {
+        if (response) this.data$.next(response);
+        else this.data$.next([]);
+      },
+      error => { 
+        this.dialog.open(DialogAlertComponent, {
+          data: {title: "Error", message: error.error}
+        });
+      }
+    ); 
+  }
+  
+  persist(): Observable<any> {
+    return this.dd.post("persist_array", this.entityName, this.serverData())
+  }
+
+  reload(response){
+    this.snackBar.open("Registro realizado", "X");
+    let route = emptyUrl(this.router.url) + "?ids="+response["ids"].join("&ids=");
+    if(route != this.router.url) this.router.navigateByUrl('/' + route, {replaceUrl: true});
+    else this.setData();
+    this.isSubmitted = false;
+  }
+
+}
---
+++ component/fieldset-array/fieldset-array.component.ts
@@ -0,0 +1,110 @@
+import { Input, OnInit, Component, AfterViewInit} from '@angular/core';
+import { FormGroup, FormControl, FormGroupDirective, NgForm, FormBuilder, FormArray } from '@angular/forms';
+import { Observable, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
+import { SessionStorageService } from '../../service/storage/session-storage.service';
+import { fastClone } from '../../function/fast-clone';
+import {ErrorStateMatcher} from '@angular/material/core';
+
+
+@Component({
+  selector: 'core-fieldset-array',
+  template: '',
+})
+export abstract class FieldsetArrayComponent implements  OnInit  {
+  /**
+   * Componente de administración de fieldset. Características:
+   *   El formulario y los datos son definidos en componente principal  
+   *   Puede inicializar datos adicionales susceptibles de ser utilizados en componentes anidados
+   */
+
+  @Input() form: FormGroup; 
+  /**
+   * Formulario padre
+   */
+
+  @Input() data$: Observable<any>; 
+  /**
+   * Datos del formulario
+   */
+
+  readonly entityName: string; 
+  /**
+   * entidad principal del componente  
+   * Utilizado solo para identificar el fieldset
+   */
+  
+  fieldset: FormArray; 
+  /**
+   * fieldset
+   */
+
+  protected subscriptions = new Subscription();
+
+  readonly defaultValues: {[key:string]: any} = {};
+
+  constructor(
+    protected router: Router, 
+    protected storage: SessionStorageService, 
+  ) { }
+
+  abstract formGroup();
+
+  formValues = this.storage.getItem(this.router.url);
+
+  ngOnInit() {    
+    /**
+     * Al inicializar el formulario se blanquean los valores del storage, por eso deben consultarse previamente
+     */
+    this.initForm();
+    this.initData();
+  }
+
+  initForm(): void {
+    this.fieldset = new FormArray([]); //secretLairs as an empty FormArray
+    this.form.addControl(this.entityName, this.fieldset);
+  }
+
+  fg(index) { return this.fieldset.controls[index]; }
+  
+  add() {
+    var fg = this.formGroup();
+    fg.reset(this.defaultValues); 
+    this.fieldset.push(fg); 
+  }
+
+  remove(index) { this.fieldset.removeAt(index); }
+
+  initData(): void {
+    /**
+     * @todo no me suscribo desde el template porque dispara errores ExpressionChanged
+     * Un posible problema es que inicializo en null y despues asigno el valor a traves de reset o patchValue
+     * Habria que ver si se puede efectuar todo el proceso de inicializacion del formulario y asignacion de valores en un mismo observable
+     * Al suscribirse directamente en el ts no dispara los errores, se carga primero el formulario y despues se asigna el valor
+     * Puede haber inconvenientes si se desea acceder al valueChanges en los subcomponentes,
+     * la asignacion de datos iniciales no sera considerada como valueChange (se puede solucionar de la misma forma suscribiendose en el ts)
+     */
+    if(this.formValues) {
+      var d = this.formValues.hasOwnProperty(this.entityName)? this.formValues[this.entityName] : null;
+      if(d) this.initValues(d);
+      this.formValues = null;
+    } else {
+      var s = this.data$.subscribe(
+        response => {
+          this.initValues(response);
+        }
+      );
+      this.subscriptions.add(s);
+    }
+  }
+
+  initValues(response: {[key:string]: any}[] = []){
+    this.fieldset.controls.length = 0; //inicializar
+    for(var i = 0; i < response.length; i++){
+      this.add();
+      var res = fastClone(response[i]);
+      this.fieldset.controls[i].reset(res);
+    }
+  }
+ 
+}
