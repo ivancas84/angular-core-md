ca52e04c96f31edf37fa51194eee873364124814
ivancas84 <ivancas84@gmail.com>
2020-11-11 08:55:21 -0300

    Evitar que tire error si no tiene padre

--- function/get-control-name.ts
+++ function/get-control-name.ts
@@ -1,6 +1,8 @@
 import { AbstractControl } from '@angular/forms';
 
 export function getControlName(c: AbstractControl): string | null {
-  const formGroup = c.parent.controls;
-  return Object.keys(formGroup).find(name => c === formGroup[name]) || null;
+  if(c.parent){
+    const formGroup = c.parent.controls;
+    return Object.keys(formGroup).find(name => c === formGroup[name]) || null;
+  }
 }
