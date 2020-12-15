fe7e2087bb611652e5e21d88b04d9fe145be36ad
ivancas84 <ivancas84@gmail.com>
2020-12-15 01:25:36 -0300

    Nuevo metodo advancedColumnData

--- service/data-definition/data-definition-tool.service.ts
+++ service/data-definition/data-definition-tool.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Display } from '@class/display';
 import { arrayColumn } from '@function/array-column';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -17,6 +18,32 @@ export class DataDefinitionToolService extends DataDefinitionService{
   ): Observable<{ [index: string]: any }[]>{
     var ids = arrayColumn(data, fieldName);
     return this.getAll(entityName, ids).pipe(
+      map(
+        response => {
+          for(var i = 0; i < data.length; i++){
+            for(var f in fields){
+              if(fields.hasOwnProperty(f)){
+                data[i][f] = response[i][fields[f]];
+              }
+            }
+          }
+          return data;
+        }
+      )
+    );  
+  }
+
+  advancedColumnData(
+    data: { [index: string]: any }[], 
+    fieldName: string, 
+    entityName: string, 
+    fields: { [index: string]: string }
+  ): Observable<{ [index: string]: any }[]>{
+    var ids = arrayColumn(data, fieldName);
+    var display = new Display();
+    display.setFields(Object.values(fields));
+    display.addCondition(["id","=",ids]);
+    return this.post("advanced",entityName, display).pipe(
       map(
         response => {
           for(var i = 0; i < data.length; i++){
