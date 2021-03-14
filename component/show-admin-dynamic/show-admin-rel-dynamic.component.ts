5aced316d4b7436f6574408b1e3e58381c50ab67
ivancas84 <ivancas84@gmail.com>
2021-03-14 11:14:29 -0300

    ShowAdminRelDynamicComponent v1: Incorporar version inicial del componente para facilitar la implementacion de grillas administrativas con relaciones

---
+++ component/show-admin-dynamic/show-admin-rel-dynamic.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { ShowAdminDynamicComponent } from './show-admin-dynamic.component';
+
+@Component({
+  selector: 'core-show-admin-rel-dynamic',
+  templateUrl: './show-admin-dynamic.component.html',
+})
+export abstract class ShowAdminRelDynamicComponent extends ShowAdminDynamicComponent { //v1
+  /**
+   * Variante de ShowAdminDynamic para utilizar campos de relaciones
+   */
+  
+   constructor(
+    protected dd: DataDefinitionToolService, 
+    protected route: ActivatedRoute, 
+    protected dialog: MatDialog,
+    protected validators: ValidatorsService, //los atributos fieldViewOptions y fieldViewOptionsFiters utilizar validadores
+    protected ddra: DataDefinitionRelArrayService
+  ) {
+    super(dd,route,dialog, validators)
+  }
+  
+  reloadApi: string = "unique_rel_array"; //reloadApi de TableAdmin
+  /**
+   * Puede utilizarse unique_rel_array
+   * si se desea administrar una entidad y sus relaciones
+   */
+
+   queryData(): Observable<any>{
+    return this.dd.post(this.queryApi, this.entityName, this.display).pipe(
+      switchMap(
+        ids => {
+          return this.ddra.main(this.entityName, ids);
+        }
+      )
+    )
+  }
+
+}
