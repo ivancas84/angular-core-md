d123c9dadff8053683e430a004ed8fe581ddb266
ivancas84 <ivancas84@gmail.com>
2021-03-14 00:09:54 -0300

    1.1 Eliminar uso de clase ShowAdmin y heredar directamente de Show. Incorporar en el constructor el atributo DataDefinitionRelArrayService

--- component/show-admin-dynamic/show-admin-dynamic.component.ts
+++ component/show-admin-dynamic/show-admin-dynamic.component.ts
@@ -1,21 +1,17 @@
-import { OnInit, Component } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { of, Observable } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
-import { Display } from '@class/display';
-import { DataDefinitionService } from '@service/data-definition/data-definition.service';
 import { MatDialog } from '@angular/material/dialog';
 import { DataDefinitionToolService } from '@service/data-definition/data-definition-tool.service';
 import { ShowComponent } from '@component/show/show.component';
 import { ValidatorsService } from '@service/validators/validators.service';
-import { ShowAdminComponent } from '@component/show-admin/show-admin.component';
 import { FieldViewOptions } from '@class/field-view-options';
+import { DataDefinitionRelArrayService } from '@service/data-definition-rel-array/data-definition-rel-array.service';
 
 @Component({
   selector: 'core-show-admin-dybamic',
   templateUrl: './show-admin-dynamic.component.html',
 })
-export abstract class ShowAdminDynamicComponent extends ShowAdminComponent {
+export abstract class ShowAdminDynamicComponent extends ShowComponent {
   /**
    * Componente general para las interfaces ShowAdmin 
    * que implementan estructuras dinamicas
@@ -41,9 +37,10 @@ export abstract class ShowAdminDynamicComponent extends ShowAdminComponent {
     protected dd: DataDefinitionToolService, 
     protected route: ActivatedRoute, 
     protected dialog: MatDialog,
+    protected ddra: DataDefinitionRelArrayService,
     protected validators: ValidatorsService, //los atributos fieldViewOptions y fieldViewOptionsFiters utilizar validadores
-  ) {
-    super(dd,route,dialog)
+    ) {
+    super(dd,route,dialog, ddra)
   }
 
 
