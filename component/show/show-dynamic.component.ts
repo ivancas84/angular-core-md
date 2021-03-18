5125326396d10c75164d152d503cfe1f6556e12d
unknown <ivancas84@gmail.com>
2021-03-18 12:55:09 -0300

    eliminar imports no utilizados

--- component/show/show-dynamic.component.ts
+++ component/show/show-dynamic.component.ts
@@ -1,11 +1,4 @@
-import { OnInit, Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { of, Observable } from 'rxjs';
-import { catchError, map, switchMap, tap } from 'rxjs/operators';
-import { Display } from '@class/display';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
-import { DataDefinitionToolService } from '@service/data-definition/data-definition-tool.service';
+import { Component } from '@angular/core';
 import { ShowComponent } from './show.component';
 import { FieldViewOptions } from '@class/field-view-options';
 
