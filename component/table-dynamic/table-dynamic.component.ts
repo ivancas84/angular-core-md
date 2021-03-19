307683fc106bdc4e9a9ccf2542f6d99b1b28dc5c
unknown <ivancas84@gmail.com>
2021-03-18 22:44:01 -0300

    TableDynamicComponent v2: Eliminar uso de TableDynamicOptions. Las opciones de TableDynamic se definen como atributos independientes

--- component/table-dynamic/table-dynamic.component.ts
+++ component/table-dynamic/table-dynamic.component.ts
@@ -1,6 +1,5 @@
 import { Input, Component, OnInit } from '@angular/core';
 import { FieldViewOptions } from '@class/field-view-options';
-import { TableDynamicOptions } from '@class/table-dynamic-options';
 import { TableComponent } from '@component/table/table.component';
 
 @Component({
@@ -11,14 +10,22 @@ import { TableComponent } from '@component/table/table.component';
   .mat-table.mat-table { min-width: 500px; }
   `],
 })
-export class TableDynamicComponent extends TableComponent implements OnInit {
-  /**
-   * Version 1
-   * Compatible con TableComponent 1.x
-   */
+export class TableDynamicComponent extends TableComponent implements OnInit { //2
   @Input() fieldsViewOptions: FieldViewOptions[]
   @Input() title: string //titulo del componente
-  @Input() options: TableDynamicOptions = new TableDynamicOptions()
+  @Input() addButtonLink: string = null;
+  @Input() addButtonQueryParams: { [index: string]: any } = {};
+  @Input() copyButton: boolean = true;
+  @Input() printButton: boolean = true;
+  @Input() sortActive: string = null;
+  @Input() sortDirection: string = "asc";
+  @Input() options: any = null; //columna opciones
+  /**
+   * Si es null no se visualiza
+   * {path, label, params, icon, data}
+   */
+
+  
      
   ngOnInit(): void {
     this.displayedColumns = []
