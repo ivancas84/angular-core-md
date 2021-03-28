1e5c5372d4a5ed13dcb383939bede131c702c2cc
ivancas84 <ivancas84@gmail.com>
2021-03-28 19:46:22 -0300

    SearchDynamicComponent 2: Eliminar uso SearchDynamicOptions y cambiar directorio. Se deben actualizar las importaciones y eliminar el uso de SearchDynamicOptions, todos los atributos de SearchDynamicOptions fueron implementados como atributos de SearchDynamicComponent

--- component/search/search-dynamic.component.ts
+++ component/search/search-dynamic.component.ts
@@ -1,22 +1,18 @@
 import { Input, Component, OnInit } from '@angular/core';
 import { FieldViewOptions } from '@class/field-view-options';
-import { SearchDynamicOptions } from '@class/search-dynamic-options';
 import { SearchComponent } from '@component/search/search.component';
 
 @Component({
   selector: 'core-search-dynamic',
   templateUrl: './search-dynamic.component.html',
 })
-export class SearchDynamicComponent extends SearchComponent implements OnInit {
-  @Input() options: { [index: string]: any } = {}
+export class SearchDynamicComponent extends SearchComponent { //2
+  
+  @Input() searchParams: boolean = true; //activar/desactivar busqueda parametros
+  @Input() searchCondition: boolean = false; //activar/desactivar busqueda condiciones
+  @Input() searchOrder: boolean = false; //activar/desactivar ordenamiento
   @Input() title: string = "Opciones"
   @Input() fieldsViewOptionsSp?: FieldViewOptions[] = []; //fields control search params
   @Input() fieldsViewOptionsSc?: FieldViewOptions[] = []; //fields control search condition
-  
-  opt: SearchDynamicOptions;
-     
-  ngOnInit(): void {
-    this.opt = new SearchDynamicOptions(this.options);
-  }
   
 }
