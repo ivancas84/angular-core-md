7fdb541d67df3f961b8d6cb646047aefcdbc16e4
ivancas84 <ivancas84@gmail.com>
2021-04-13 21:16:50 -0300

    ShowRelDynamicComponent 1.3: Implementar DataDefinitionToolService 2.5

--- component/show/show-rel-dynamic.component.ts
+++ component/show/show-rel-dynamic.component.ts
@@ -12,7 +12,7 @@ import { isEmptyObject } from '@function/is-empty-object.function';
   selector: 'core-show-rel-dynamic',
   template: '',
 })
-export abstract class ShowRelDynamicComponent extends ShowDynamicComponent { //1.2
+export abstract class ShowRelDynamicComponent extends ShowDynamicComponent { //1.3
   constructor(
     protected dd: DataDefinitionToolService, 
     protected route: ActivatedRoute, 
@@ -25,20 +25,7 @@ export abstract class ShowRelDynamicComponent extends ShowDynamicComponent { //1
   queryData(): Observable<any>{
     return this.dd.post("ids", this.entityName, this.display).pipe(
       switchMap(
-        ids => {
-          var fields = {};
-          for(var i = 0; i < this.fieldsViewOptions.length; i++){
-            var f = this.fieldsViewOptions[i].field;
-            if(f.includes("-")){
-              var n = f.indexOf("-");
-              fields[f]= f.substring(n+1)
-            }
-          }
-          return (isEmptyObject(fields)) ? 
-            this.dd.getAll(this.entityName, ids) : 
-            this.dd.relGetAll(this.entityName, ids, fields);
-          
-        }
+        ids => this.dd.relGetAllFvo(this.entityName, ids, this.fieldsViewOptions)
       )
     )
   }
