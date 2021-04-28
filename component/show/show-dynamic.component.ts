9c1878e33af6a9ff0e9619acd6725e4a256da5c0
ivancas84 <ivancas84@gmail.com>
2021-04-28 11:47:46 -0300

    ShowDynamicComponent 3:Incorporar soporte para titulos del componente anidado TableDynamic

--- component/show/show-dynamic.component.ts
+++ component/show/show-dynamic.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ShowComponent } from './show.component';
 import { FieldViewOptions } from '@class/field-view-options';
 
@@ -6,7 +6,7 @@ import { FieldViewOptions } from '@class/field-view-options';
   selector: 'core-show-dynamic',
   template: '',
 })
-export abstract class ShowDynamicComponent extends ShowComponent { //2
+export abstract class ShowDynamicComponent extends ShowComponent implements OnInit { //3
   fieldsViewOptions: FieldViewOptions[] = []
   fieldsViewOptionsSp: FieldViewOptions[] = []
   optColumn: any[] = null; //columna opciones (si es null no se visualiza)
@@ -14,10 +14,17 @@ export abstract class ShowDynamicComponent extends ShowComponent { //2
    * Cada elemento debe ser uno de los siguientes OptRouteIcon | OptLinkIcon | OptRouteText | OptLinkText
    */
 
-   switchAction($event:any){ throw new Error("Not Implemented"); }
+  title: string = null //titulo de la tabla
+
+  ngOnInit(): void {
+    if(this.title === null) this.title = this.entityName;
+    super.ngOnInit();
+  }
+
+  switchAction($event:any){ throw new Error("Not Implemented"); }
   /**
    * Sobescribir si se necesita utilizar eventos
    * Utilizar $event.action para la accion a ejecutar (corresponde a opt.action)
    * Utilizar $event.data para los datos a utilizar (corresponde a row)
-   */
+   */ 
 }
