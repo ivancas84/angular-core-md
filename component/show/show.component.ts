581abedb3304402255b90f551316830560d1e961
ivancas84 <ivancas84@gmail.com>
2021-05-09 09:25:28 -0300

    ShowComponent 1.1: Uso de valor undefined para atributo length

--- component/show/show.component.ts
+++ component/show/show.component.ts
@@ -6,21 +6,23 @@ import { Display } from '@class/display';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAlertComponent } from '@component/dialog-alert/dialog-alert.component';
 import { DataDefinitionToolService } from '@service/data-definition/data-definition-tool.service';
+import { SessionStorageService } from '@service/storage/session-storage.service';
+import { fastClone } from '@function/fast-clone';
 
 @Component({
   selector: 'core-show',
   template: '',
 })
-export abstract class ShowComponent implements OnInit {
+export abstract class ShowComponent implements OnInit { //1.1
   /**
    * Grilla de visualizacion
    */
 
   readonly entityName: string; //Nombre de la entidad principal
   data: any; //datos principales
-  length: number = null; //longitud total de los datos a mostrar
+  length?: number; //longitud total de los datos a mostrar
   /**
-   * false: No se procesara la longitud
+   * undefined: No se procesara la longitud
    * null: Se considera que no hay datos
    */
   
@@ -33,6 +35,7 @@ export abstract class ShowComponent implements OnInit {
     protected dd: DataDefinitionToolService, 
     protected route: ActivatedRoute, 
     protected dialog: MatDialog,
+    protected storage: SessionStorageService
   ) {}
 
   ngOnInit(): void {
@@ -66,7 +69,7 @@ export abstract class ShowComponent implements OnInit {
 
   initLength(): Observable<any> {
     /**
-     * Si no se desea procesar la longitud, retornar valor false return of(false)
+     * Si no se desea procesar la longitud, retornar of(undefined)
      */
     return this.dd.post("count", this.entityName, this.display).pipe(
       catchError(
@@ -100,7 +103,6 @@ export abstract class ShowComponent implements OnInit {
       tap(
         data => {
           this.data = data;
-
         }
       ),      
     )
@@ -108,6 +110,36 @@ export abstract class ShowComponent implements OnInit {
 
   queryData(): Observable<any>{
     return this.dd.all(this.entityName, this.display)
+  }
+
+  delete(id){
+    /**
+     * Importante, utilizar el id
+     * En implementaciones previas, se utilizaba el indice y se eliminaba el contenido del array para evitar consultar a la base de datos
+     * Pero si se utilizaba ordenamiento de tablas, no se reflejaba el indice correcto al eliminar
+     * @todo Reimplementar para evitar acceder al servidor una vez eliminado
+     * No es conveniente implementar alguna solucion desde los componentes anidados?
+     */
+    this.dd._post("delete",this.entityName,[id]).pipe(
+      map(
+        response => {
+          this.storage.removeItemsContains(".");
+          this.storage.removeItemsPersisted(response["detail"]);
+        }
+      ),
+      switchMap(
+        () => {
+          this.length--;
+          return this.initData()
+        }
+      )
+    ).subscribe(
+      () => {
+        this.dialog.open(DialogAlertComponent, {
+          data: {title: "Registro eliminado", message: "Se ha eliminado un registro"}
+        })
+      }
+    )
   }
 
 }
