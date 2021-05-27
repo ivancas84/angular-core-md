1be699a9438849d3cc9857f6609bd39f42183fcb
ivancas84 <ivancas84@gmail.com>
2021-05-27 08:59:14 -0300

    Completar documentacion interna

--- class/fieldset-dynamic-options.ts
+++ class/fieldset-dynamic-options.ts
@@ -1,4 +1,9 @@
 export class FieldsetDynamicOptions {
+  /**
+   * Opciones de FieldsetDynamicComponent
+   * Definir una clase de opciones facilita la asignacion de valores.
+   * Es util para aquellas estructuras donde se manipulan diferentes tipos de componentes y cada una tiene su juego de opciones y valores por defecto definidos.
+   */
   inputSearchGo: boolean = true;
 
   constructor(attributes: any = {}) {
