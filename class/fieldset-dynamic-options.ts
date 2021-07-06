bbf76ae08389b1a34466b35529de96bdb6670e64
ivancas84 <ivancas84@gmail.com>
2021-07-06 11:12:23 -0300

    Incorporar nuevas caracteristicas

--- class/fieldset-dynamic-options.ts
+++ class/fieldset-dynamic-options.ts
@@ -1,10 +1,15 @@
+import { ValidatorOpt } from "./validator-opt";
+
 export class FieldsetDynamicOptions {
   /**
    * Opciones de FieldsetDynamicComponent
    * Definir una clase de opciones facilita la asignacion de valores.
    * Es util para aquellas estructuras donde se manipulan diferentes tipos de componentes y cada una tiene su juego de opciones y valores por defecto definidos.
    */
-  inputSearchGo: boolean = true;
+
+  inputSearchGo: boolean = true //activar o desactivar el formulario de busqueda rapida
+  validators: ValidatorOpt[] = [] //opciones de validacion del fieldset
+  intro?:string //parrafo con un mensaje de introduccion al formulario, util para indicar instrucciones breves
 
   constructor(attributes: any = {}) {
     for(var a in attributes){
