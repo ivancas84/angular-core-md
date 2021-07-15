a6737937f362b656c9b8ea4cfa63c88ed9a8d384
ivancas84 <ivancas84@gmail.com>
2021-07-15 13:21:25 -0300

    Pruebas iniciales de encadenamiento de observables a traves de funciones de recursion para eliminar las iteraciones

--- service/data-definition/data-definition-fk-obj.service.ts
+++ service/data-definition/data-definition-fk-obj.service.ts
@@ -159,8 +159,12 @@ export class DataDefinitionFkObjService {
      * @param tree Arbol de relaciones
      * @param relationsFk Relaciones que deben inicializarse
      */
-    var obs;
+    var keys = Object.keys(tree);
+    return this.chaining(id, row, tree, relationsFk, keys, 0)
+/*
     for(var key in tree){
+      
+        DEPRECATED?
       if(relationsFk.includes(key)){
         if(!obs) obs = this.recursive(id, row, key, tree, relationsFk);
         else obs.pipe(
@@ -173,6 +177,21 @@ export class DataDefinitionFkObjService {
       }
     }
     return (obs) ? obs : of(row);
+    }*/
   }
 
+  protected chaining(id, row, tree, relationsFk, keys, i){
+    if(i == keys.length) return of(row);
+    else return this.subchaining(id, row, tree, relationsFk, keys[i]).pipe(
+      switchMap(
+        row => this.chaining(id, row, tree, relationsFk, keys, ++i)
+      )
+    )
+  }
+
+
+  protected subchaining(id, row, tree, relationsFk, key){
+    if(!relationsFk.includes(key)) return of(row);
+    return this.recursive(id, row, key, tree, relationsFk)
+  }
 }
