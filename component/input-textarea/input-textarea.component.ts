1b6bd559426bd9615db9fb05cf31bc1f9d89335e
ivancas84 <ivancas84@gmail.com>
2021-07-16 13:06:47 -0300

    Adaptar a la nueva estructura de validacion

--- component/input-textarea/input-textarea.component.ts
+++ component/input-textarea/input-textarea.component.ts
@@ -1,5 +1,6 @@
 import { Input, OnInit, Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { AsyncValidatorOpt, ValidatorOpt } from '@class/validator-opt';
 import { getControlName } from '@function/get-control-name';
 
 @Component({
@@ -10,29 +11,25 @@ export class InputTextareaComponent implements OnInit { //2
   @Input() field: FormControl;
   @Input() title?: string;
   @Input() placeholder?: string = "";
-  @Input() uniqueRoute?: string //Ruta de administracion para valor unico
-  /**
-   * si no se define no se activa enlace
-   */
+  @Input() readonly?: boolean = false;
+  @Input() validatorOpts?: ValidatorOpt[] = [] //validators
+  @Input() asyncValidatorOpts?: AsyncValidatorOpt[] = [] //validators
 
-  @Input() uniqueParam?: string 
-  /**
-   * Por defecto se utiliza el metodo getControlName para definir el nombre
-   *  getControlName funciona solo si tiene padre
-   */
+  uniqueValue: string;
 
-  queryParams = {};
-
+  
   ngOnInit(): void {
-      if(!this.uniqueRoute) return;
-      if(!this.uniqueParam) this.uniqueParam = getControlName(this.field)
-      if(this.uniqueParam) this.field.valueChanges.subscribe(
-        value => {
-          this.queryParams[this.uniqueParam] = value;
-        }
-      )
-  
-
-  }
-  
+    this.field.statusChanges.subscribe(
+       () => {
+         if(this.field.hasError("notUnique")){
+           this.uniqueValue = this.field.getError("notUnique");
+           for(var i = 0; i < this.asyncValidatorOpts.length; i++){
+             if(this.asyncValidatorOpts[i].id=="notUnique") 
+               this.asyncValidatorOpts[i]["queryParams"][this.asyncValidatorOpts[i]["uniqueParam"]] = this.uniqueValue
+           }
+         }
+       }
+     );
+     
+   }
 }
