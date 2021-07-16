855b20333ca4c4f3e940eaf575e13c0ec20463c0
ivancas84 <ivancas84@gmail.com>
2021-07-16 13:06:03 -0300

    Renombrar componentes

---
+++ component/admin-array/admin-array-dynamic.component.ts
@@ -0,0 +1,22 @@
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+import { Component } from '@angular/core';
+import { Display } from '@class/display';
+import { AdminArrayComponent } from './admin-array.component';
+import { FieldViewOptions } from '@class/field-view-options';
+
+@Component({
+  selector: 'core-admin-array',
+  template: '',
+})
+export abstract class AdminArrayDynamicComponent extends AdminArrayComponent { //2
+/**
+ * Formulario de administracion para un conjunto de datos
+ * Es similiar a AdminComponent pero tiene ciertas restricciones para trabajar con un conjunto de datos
+ */
+
+ title: string;
+ fieldsViewOptions: FieldViewOptions[]
+
+
+}
