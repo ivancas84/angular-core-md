2be31b92c78ab8680086b044ba38814ea138a349
ivancas84 <ivancas84@gmail.com>
2021-07-18 16:00:49 -0300

    Implementar nuevas caracteristicas de validacion en input-select-param

--- component/input-select-param/input-select-param.component.ts
+++ component/input-select-param/input-select-param.component.ts
@@ -2,6 +2,7 @@ import { Input, OnInit, Component } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
 import { DataDefinitionService } from '@service/data-definition/data-definition.service';
+import { AsyncValidatorOpt, ValidatorOpt } from '@class/validator-opt';
 
 @Component({
   selector: 'core-input-select-param',
@@ -17,13 +18,30 @@ export class InputSelectParamComponent implements OnInit {
   @Input() field: FormControl;
   @Input() options: any[];
   @Input() title?: string;
+  @Input() readonly?: boolean = false;
+  @Input() validatorOpts?: ValidatorOpt[] = [] //validators
+  @Input() asyncValidatorOpts?: AsyncValidatorOpt[] = [] //validators
 
   options$: Observable<Array<any>>;
 
   constructor( public dd: DataDefinitionService ) { }
 
+  uniqueValue: string;
+
   ngOnInit(): void {
     if(!this.title) this.title = "Seleccione";
+
+    this.field.statusChanges.subscribe(
+      () => {
+        if(this.field.hasError("notUnique")){
+          this.uniqueValue = this.field.getError("notUnique");
+          for(var i = 0; i < this.asyncValidatorOpts.length; i++){
+            if(this.asyncValidatorOpts[i].id=="notUnique") 
+              this.asyncValidatorOpts[i]["queryParams"][this.asyncValidatorOpts[i]["uniqueParam"]] = this.uniqueValue
+          }
+        }
+      }
+    );
   }
 
 }
