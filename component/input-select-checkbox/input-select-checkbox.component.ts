e47e0f1f7e44cd6e2df3c24db08e1492e10a8f5d
ivancas84 <ivancas84@gmail.com>
2021-12-02 09:23:54 -0300

    Correcciones varias

--- component/input-select-checkbox/input-select-checkbox.component.ts
+++ component/input-select-checkbox/input-select-checkbox.component.ts
@@ -1,15 +1,17 @@
-import { Input, Component } from '@angular/core';
+import { Input, Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { ControlComponent, FormControlConfig } from '@class/reactive-form-config';
+import { getControlName } from '@function/get-control-name';
 
 export class InputSelectCheckboxConfig extends FormControlConfig {
   componentId: string = "input_select_checkbox"
+  readonly: boolean = false;
   options: any[] = ["Sí", "No"];
   /**
    * El primer valor sera transformado al string "true"
    * El segundo a "false"
    */
-  label: string = "Seleccione";
+  label: string;
 
   constructor(attributes: any = {}) {
     super(attributes)
@@ -25,7 +27,7 @@ export class InputSelectCheckboxConfig extends FormControlConfig {
   selector: 'core-input-select-checkbox',
   templateUrl: './input-select-checkbox.component.html',
 })
-export class InputSelectCheckboxComponent implements ControlComponent {
+export class InputSelectCheckboxComponent implements ControlComponent, OnInit {
   /**
    * Componente de administración de fieldset. Características:
    *   El formulario y los datos son definidos en componente principal  
@@ -35,7 +37,7 @@ export class InputSelectCheckboxComponent implements ControlComponent {
   @Input() config: InputSelectCheckboxConfig;
   @Input() control: FormControl;
 
-  isEven(n){
-    return n % 2 == 0;
+  ngOnInit(): void {
+    if(!this.config.label) this.config.label = getControlName(this.control)
   }
 }
