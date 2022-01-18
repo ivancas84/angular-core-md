3e3e17d160e0a96c18f4f9060082045b75ee279c
ivancas84 <ivancas84@gmail.com>
2022-01-18 10:18:28 -0300

    Simplificar constructores

--- class/validator-msg.ts
+++ class/validator-msg.ts
@@ -4,11 +4,7 @@ export class ValidatorMsg {
   message:string //mensaje a visualizar
 
   constructor(attributes: any = {}) {
-    for(var a in attributes){
-      if(attributes.hasOwnProperty(a)){
-        this[a] = attributes[a]
-      }
-    }
+    Object.assign(this, attributes)
   }
 }
 
@@ -19,55 +15,90 @@ export class UniqueValidatorMsg extends ValidatorMsg {
   queryParams = {} //uniqueParam se asigna a queryParams con el valor del error, adicionalmente se pueden definir queryParams alternativos
   message:string = "El valor ya se encuentra en uso" //mensaje a visualizar
 
-
   constructor(attributes: any = {}) {
-    super(attributes);
-    for(var a in attributes){
-      if(attributes.hasOwnProperty(a)){
-        this[a] = attributes[a]
-      }
-    }
+    super({})
+    Object.assign(this, attributes)
   }
 }
 
 export class RequiredValidatorMsg extends ValidatorMsg {
   id:string = "required" //id del validador (debe ser el mismo que el identificador del error retornado)
   message:string = "Debe completar valor" //mensaje a visualizar
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 }
 
 export class DateValidatorMsg extends ValidatorMsg {
   id:string = "matDatepickerParse"
   message:string = "El formato es incorrecto"
+  
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 }
 
 export class PatternValidatorMsg extends ValidatorMsg {
   id:string = "pattern"
   message:string = "El formato es incorrecto"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 }
 
 export class MinValidatorMsg extends ValidatorMsg {
   id:string = "min"
   message:string = "Inferior al mínimo permitido"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 }
 
 
 export class MaxValidatorMsg extends ValidatorMsg {
   id:string = "max"
   message:string = "Superior al máximo permitido"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 }
 
 export class EmailValidatorMsg extends ValidatorMsg {
   id:string = "email"
   message:string = "El formato es incorrecto"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 } 
 
 export class MinLengthValidatorMsg extends ValidatorMsg {
   id:string = "minlength"
   message:string = "Longitud incorrecta"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 } 
 
 export class MaxLengthValidatorMsg extends ValidatorMsg {
   id:string = "maxlength"
   message:string = "Longitud incorrecta"
+
+  constructor(attributes: any = {}) {
+    super({})
+    Object.assign(this, attributes)
+  }
 } 
 
