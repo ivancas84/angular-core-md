8b7906b27083482b5aa8a471d7689c3809b68c78
ivancas84 <ivancas84@gmail.com>
2022-05-13 13:45:03 -0300

    Incorporar inicializacion de valores por defecto

--- component/field-wrap-card/field-wrap-card.component.ts
+++ component/field-wrap-card/field-wrap-card.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { FormControlConfig } from '@class/reactive-form-config';
 import { FieldWrapComponent } from '@component/field-wrap/field-wrap.component';
-
+import { getControlName } from '@function/get-control-name';
+import { titleCase } from '@function/title-case';
 
 export class FieldWrapCardConfig extends FormControlConfig {
   override component: any = FieldWrapCardComponent
@@ -13,7 +14,15 @@ export class FieldWrapCardConfig extends FormControlConfig {
   selector: 'core-field-wrap-card',
   templateUrl: './field-wrap-card.component.html',
 })
-export class FieldWrapCardComponent extends FieldWrapComponent {
-  @Input() override config!: FieldWrapCardConfig;
+export class FieldWrapCardComponent extends FieldWrapComponent implements OnInit {
+
+  @Input() override config: FieldWrapCardConfig | {[key:string]: any} = {};
+
+  ngOnInit(): void {
+    if(!this.config.hasOwnProperty("component")) this.config = new FieldWrapCardConfig(this.config)
+
+    var n = getControlName(this.control)
+    this.config.label = titleCase(n!.substring(n!.indexOf("-")+1).replace("_"," "))
+  }
  
 }
