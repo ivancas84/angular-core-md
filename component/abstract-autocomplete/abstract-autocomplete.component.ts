f2a19c6fdfb6e381273c758553b8ea83bd8ad77c
ivancas84 <ivancas84@gmail.com>
2022-11-17 13:37:24 -0300

    Agregar componente abstracto con comportamiento habitual para un autocomplete independiente

---
+++ component/abstract-autocomplete/abstract-autocomplete.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ComponentToolsService } from '@service/component-tools/component-tools.service';
+import { map, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-abstract-autocomplete',
+  template: ''
+})
+export abstract class AbstractAutocompleteComponent implements OnInit {
+
+  entityName!: string;
+  title: string = "";
+
+
+  @Input() control!: FormControl //control
+
+  loadAutocomplete$!: Observable<any>; //inicializar
+  searchControl: FormControl = new FormControl(null); //control para busqueda, no interfiere con el control del fieldset principal
+  filteredOptions$!: Observable<Array<{ [key: string]: any; }>>; //opciones de busqueda
+  label: string = ""; //label para mostrar si hay inicializado un valor
+  
+  constructor(
+    protected tools: ComponentToolsService,
+  ) { }
+
+
+  ngOnInit(): void {
+    this.filteredOptions$ = this.tools.filteredOptionsAutocomplete({
+      entityName:this.entityName,
+      control:this.control,
+      searchControl:this.searchControl,
+    })
+ 
+    this.loadAutocomplete$ = this.tools.labelAutocomplete({
+      entityName:this.entityName,
+      control:this.control,
+      searchControl:this.searchControl,
+    }).pipe(
+         map(
+           label => {
+             this.label = label;
+             return true;
+           }
+         )
+       )
+
+  }
+
+}
