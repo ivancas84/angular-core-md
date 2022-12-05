86757ced6252f904ebdf7df6be830ab67a87bc3e
ivancas84 <ivancas84@gmail.com>
2022-12-05 13:26:35 -0300

    Corregir error en pipe summary

--- pipe/summary.pipe.ts
+++ pipe/summary.pipe.ts
@@ -2,5 +2,5 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: "summary" })
 export class SummaryPipe implements PipeTransform {
-    transform(value: string, length: number): string { return (value.length < length) ? 'Sí' : value.substring(0,length); };
+    transform(value: string, length: number): string { return (!value || value.length < length) ? value : value.substring(0,length) + " ... "; };
 }
